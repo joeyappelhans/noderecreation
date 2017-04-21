bfb4871f3459dce59a9d28ac3ed895128062ae2f
joeyappelhans <9jappelhans@stpatrick.org>
2017-04-21 13:58:39 -0500

    started hover

--- hover.js
+++ hover.js
@@ -1,5 +1,24 @@
 (function)(){
     const doc=document;
-    let pic1=doc.querySelector("#first div div img");
-    pic1.addEventListener("hover",function)
+    let img1=doc.querySelector("#first div img");
+    let leftdiv=doc.querySelector("#left");
+    let rightdiv=doc.querySelector("#right");
+    img1.addEventListener("mouseover",function(){
+        img1.style.border="13px solid green";
+    })
+    img1.addEventListener("mouseout",function(){
+        img1.style.border="13px solid transparent";
+    })
+    leftdiv.addEventListener("mouseover",function(){
+        leftdiv.style.opacity="0.8";
+    })
+    leftdiv.addEventListener("mouseout"funtion(){
+        leftdiv.style.opacity="1";
+    })
+    rightdiv.addEventListener("mouseover",function(){
+        rightdiv.style.opacity="0.8";
+    })
+    rightdiv.addEventListener("mouseout"funtion(){
+        rightdiv.style.opacity="1";
+    })
 }
